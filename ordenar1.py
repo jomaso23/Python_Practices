2468934fe3cf88a149010e242ba59349f3e0c8c9
jomaso23 <120762787+jomaso23@users.noreply.github.com>
2023-03-31 19:31:45 -0300

    Add files via upload
    array sort on python

---
+++ ordenar1.py
@@ -0,0 +1,6 @@
+﻿my_list = [8, 10, 6, 2, 4]  # lista a ordenar
+
+for i in range(len(my_list) - 1):  # necesitamos (5 - 1) comparaciones
+    if my_list[i] > my_list[i + 1]:  # compara elementos adyacentes
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]  # Si terminamos aquí, tenemos que intercambiar elementos.
+
